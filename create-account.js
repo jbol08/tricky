d4fd698d5503f8b6063d36fb9d1e8c54136bd7c9
Jose Bolanos <bolanosjjr@gmail.com>
2021-05-14 21:27:43 -0500

    done

--- create-account.js
+++ create-account.js
@@ -1,5 +1,28 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin, amount = 0) {
+    //need to return an object with 4 methods
+    //so create a return function that has each method
+    return {
+        checkBalance(input) {
+            if (input !== pin) return "Invalid PIN.";
+            return `$${amount}`;
+        },
+        deposit(input, newAmount) {
+            if (input !== pin) return "Invalid PIN.";
+            amount = amount + newAmount;
+            return `Succesfully deposited $${newAmount}. Current balance: $${amount}.`;
+        },
+        withdraw(input, withdrawalAmount) {
+            if (input !== pin) return "Invalid PIN.";
+            if (withdrawalAmount > amount) return 'Withdrawal amount exceeds account balance. Transaction cancelled.';
+            amount = amount - withdrawalAmount;
+            return `Succesfully withdrew $${withdrawalAmount}. Current balance: $${amount}.`
+        },
+        changePin(input, newPin) {
+            if (input !== pin) return "Invalid PIN.";
+            pin = newPin;
+            return 'PIN successfully changed!';
+        }
+    }
 }
 
 module.exports = { createAccount };
--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,13 @@
 function curriedAdd(total) {
-
+    //add all numbers passed in
+    //if nothing passed in then return 0
+    if (total === undefined || total === null) return 0;
+    //return a function that adds numbers together
+    return function addNums(nums) {
+        if (nums === undefined || nums === null) return total;
+        total = total + nums;
+        return addNums;
+    };
 }
 
 module.exports = { curriedAdd };
